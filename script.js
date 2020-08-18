89598dd2d29bd3c50c825abcebe4f941f641f0bd
Eloisefire <sosotitan@gmail.com>
2020-08-17 19:17:46 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+
+const button = document.getElementById('button');
+const audioElement = document.getElementById('audio');
+
+function toggleButton() {
+    button.disabled = !button.disabled;
+}
+
+function tellMe(joke) {
+    // console.log("tell me:", joke);
+    VoiceRSS.speech({
+        key: '8cf173dc49b642c3abc43ad1a97fb1d3',
+        src: joke,
+        hl: 'en-ca',
+        // v: 'Rose',
+        r: 0,
+        c: 'mp3',
+        f: '44khz_16bit_stereo',
+        ssml: false
+    });
+    
+}
+
+async function getJokes() {
+    let joke = '';
+    const apiUrl = 'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist';
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        if (data.setup) {
+            joke = `${data.setup} ... ${data.delivery}`;
+        } else {
+            joke = data.joke;
+        }
+       
+        tellMe(joke);
+        toggleButton();
+        // console.log(joke);
+    } catch(error) {
+        console.log("lmfao", error)
+    }
+}
+
+button.addEventListener('click', getJokes);
+audioElement.addEventListener('ended', toggleButton);
+
+
